918b563b87b9f55f918b28fb0db845d4fc25e4f0
Batbayar Batsukh <batbayarbatsukh@Batbayars-MacBook-Pro.local>
2024-03-27 19:26:36 +0800

    1

---
+++ front-end/src/pages/components/ResetNewPasswordPageTwo.tsx
@@ -0,0 +1,90 @@
+import { Box, Stack } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import ResetNewPassword from "./ResetNewPassword";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+export default function ResetNewPasswordPageTwo() {
+  const router = useRouter();
+  const rePassSendedCode: number = 1234;
+  const [resetPincode, setResetPincode] = useState<any>();
+  const [showPinCode, setShowPinCode] = useState<boolean>(false);
+  const [changePinMode, setChangePinMode] = useState<boolean>(false);
+  const email: string = "hafel@gmail.com";
+  return (
+    <Box
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <Stack
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          width: "384px",
+          padding: "32px",
+        }}
+        direction="column"
+        spacing={6}
+      >
+        <h1 style={{ font: "San Francisco" }}>Нууц үг сэргээх</h1>
+        <Stack sx={{ width: "100%" }} direction="column">
+          <p style={{ display: "flex", gap: "4px" }}>
+            Таны
+            {<span style={{ color: "#18BA51" }}>{email}</span>}
+            хаяг руу сэргээх код илгээх болно.
+          </p>
+          <InputLabel shrink htmlFor="bootstrap-input">
+            Нууц үг сэргээх код
+          </InputLabel>
+          <Stack sx={{ position: "relative" }}>
+            <TextField
+              sx={{ backgroundColor: "#ECEDF0" }}
+              id="outlined-basic"
+              variant="outlined"
+              placeholder="******"
+              type={showPinCode ? "text" : "password"}
+              onChange={(e) => {
+                setResetPincode(e.target.value);
+              }}
+            />
+            {changePinMode ? (
+              <VisibilityOffIcon
+                sx={{ position: "absolute", right: "10px", top: "30%" }}
+                onClick={() => {
+                  setShowPinCode(!showPinCode);
+                  setChangePinMode(!changePinMode);
+                }}
+              />
+            ) : (
+              <VisibilityIcon
+                sx={{ position: "absolute", right: "10px", top: "30%" }}
+                onClick={() => {
+                  setShowPinCode(!showPinCode);
+                  setChangePinMode(!changePinMode);
+                }}
+              />
+            )}
+          </Stack>
+        </Stack>
+        <Button
+          onClick={() => {
+            if (rePassSendedCode == resetPincode) {
+              router.push("./CreateNewPassword");
+            }
+          }}
+          style={{
+            width: "100%",
+            height: "48px",
+            backgroundColor: "#18BA51",
+            color: "#fff",
+            border: "nones",
+          }}
+        >
+          Үргэлжлүүлэх
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,9 +1,56 @@
-import ResponsiveAppBar from "@/components/header";
-
+import * as React from "react";
+import { alpha, styled } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import { OutlinedInputProps } from "@mui/material/OutlinedInput";
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  "label + &": {
+    marginTop: theme.spacing(3),
+  },
+  "& .MuiInputBase-input": {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.mode === "light" ? "#F3F6F9" : "#1A2027",
+    border: "1px solid",
+    borderColor: theme.palette.mode === "light" ? "#E0E3E7" : "#2D3843",
+    fontSize: 16,
+    width: "auto",
+    padding: "10px 12px",
+    transition: theme.transitions.create([
+      "border-color",
+      "background-color",
+      "box-shadow",
+    ]),
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:focus": {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}));
 export default function Home() {
   return (
     <div>
-        <ResponsiveAppBar />
+      <FormControl variant="standard">
+        <InputLabel shrink htmlFor="bootstrap-input">
+          Имэйл
+        </InputLabel>
+        <BootstrapInput defaultValue="Имэйл хаягаа оруулна уу" />
+      </FormControl>
     </div>
-  )
+  );
 }
