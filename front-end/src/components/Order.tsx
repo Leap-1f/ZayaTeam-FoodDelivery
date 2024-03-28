15ed456a4f4325f5e8fda47ae64c5ab2df474060
Munkhzaya Baatarkhuu <b.mnkhzaya@gmail.com>
2024-03-28 21:00:33 +0800

    sags duusagui

--- front-end/src/components/Order.tsx
+++ front-end/src/components/Order.tsx
@@ -132,7 +132,7 @@ export default function Order() {
             }}
           >
             <HeaderBucketIcon />
-            Сагс
+            Сагс  
           </Box>
           <Drawer
             anchor={anchor}
