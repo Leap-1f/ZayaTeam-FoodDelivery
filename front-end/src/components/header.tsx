16a065a660a3ad2917bb257ded6b5f9aa14ee1db
Munkhzaya Baatarkhuu <b.mnkhzaya@gmail.com>
2024-03-28 20:58:34 +0800

    zahialga heseg hiij bna duusagui

--- front-end/src/components/header.tsx
+++ front-end/src/components/header.tsx
@@ -1,61 +1,65 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import PineconeHeaderLogo from './svg/pineconeheaderlogosvg';
-import HeaderLogInIcon from './svg/headerloginiconsvg';
-import HeaderBucketIcon from './svg/headerbucketiconsvg';
-import { Rowing } from '@mui/icons-material';
-import { Search } from '@mui/icons-material';
-import SearchIcon from '@mui/icons-material/Search';
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import PineconeHeaderLogo from "./svg/pineconeheaderlogosvg";
+import HeaderLogInIcon from "./svg/headerloginiconsvg";
+import HeaderBucketIcon from "./svg/headerbucketiconsvg";
+import { Rowing } from "@mui/icons-material";
+import { Search } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import AnchorTemporaryDrawer from "./Order";
 
-
-const pages = ['НҮҮР', 'ХООЛНЫ ЦЭС', 'ХҮРГЭЛТИЙН БҮС'];
+const pages = ["НҮҮР", "ХООЛНЫ ЦЭС", "ХҮРГЭЛТИЙН БҮС"];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
 
-  const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
+    "&:hover": {
       backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
-      width: 'auto',
+      width: "auto",
     },
   }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   }));
-  
+
   const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    width: '100%',
+    color: "inherit",
+    width: "100%",
   }));
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -70,48 +74,69 @@ function ResponsiveAppBar() {
     setAnchorElUser(null);
   };
 
-
-
   return (
-    <AppBar position="static" sx={{backgroundColor: "#FFFFFF"}}>
+    <AppBar position="static" sx={{ backgroundColor: "#FFFFFF" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          
-            <PineconeHeaderLogo />
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            </Box>
-            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', fontFamily: "SF Pro Text" } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: '#000000', display: 'block', marginLeft: '20px'}}
-                >
-                  {page}
-                </Button>
-              ))}
-            
+          <PineconeHeaderLogo />
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}></Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex", fontFamily: "SF Pro Text" },
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  color: "#000000",
+                  display: "block",
+                  marginLeft: "20px",
+                }}
+              >
+                {page}
+              </Button>
+            ))}
           </Box>
-          <Search sx={{ border: 'solid', borderRadius: '8px', borderColor: '#000000', marginRight: '30px'}}>
-            <SearchIconWrapper >
-              <SearchIcon sx={{color: '#000000', marginLeft: '-5px', cursor: 'pointer'}}/>
+          <Search
+            sx={{
+              border: "solid",
+              borderRadius: "8px",
+              borderColor: "#000000",
+              marginRight: "30px",
+            }}
+          >
+            <SearchIconWrapper>
+              <SearchIcon
+                sx={{ color: "#000000", marginLeft: "-5px", cursor: "pointer" }}
+              />
             </SearchIconWrapper>
             <StyledInputBase
-              placeholder="Хайх" sx={{color: '#000000', width: '180px', marginLeft: '40px'}}
-              inputProps={{ 'aria-label': 'search' }}
+              placeholder="Хайх"
+              sx={{ color: "#000000", width: "180px", marginLeft: "40px" }}
+              inputProps={{ "aria-label": "search" }}
             />
           </Search>
-          <Box sx={{display: 'flex', gap: '40px'}}>
-            <Box sx={{color: "#000000", fontFamily: "sans-serif", display: 'flex', alignItems: 'center', gap:'10px', cursor: 'pointer'}}>   
-                  <HeaderBucketIcon/>
-                  Сагс
+          <Box sx={{ display: "flex", gap: "40px" }}>
+            <AnchorTemporaryDrawer />
+            <Box
+              sx={{
+                color: "#000000",
+                fontFamily: "sans-serif",
+                display: "flex",
+                alignItems: "center",
+                gap: "10px",
+                cursor: "pointer",
+              }}
+            >
+              <HeaderLogInIcon />
+              Нэвтрэх
             </Box>
-            <Box sx={{color: "#000000", fontFamily: "sans-serif", display: 'flex', alignItems: 'center', gap: '10px', cursor: 'pointer'}}>            
-                  <HeaderLogInIcon />
-                  Нэвтрэх
-            </Box>  
-          </Box>  
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -6,6 +6,8 @@ export default function Home() {
   return (
     <>
       <ResponsiveAppBar />
+      <RecipeReviewCard />
+      <OrderDetail />
     </>
   );
 }
