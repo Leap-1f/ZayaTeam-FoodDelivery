73224707991819cb2b1a7d7889cd60c5f605727f
Munkhzaya Baatarkhuu <b.mnkhzaya@gmail.com>
2024-04-05 19:53:30 +0800

    ..

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import AdminSideBar from "@/components/adminDashboard/AdminMenu";
-=======
 import { getUserLayout } from "@/components/layout/UserLayout";
->>>>>>> a9d210c (admindashboard yznlav)
 
 export default function Home() {
-  return (
-    <>
-      <AdminSideBar />
-    </>
-  );
+  return <></>;
 }
 Home.getLayout = getUserLayout;
