3db288dbabea7373de668b018a926eefc34c406e
batuka228 <batukagshhh@gmail.com>
2024-04-05 16:23:55 +0800

    bug

--- back-end/model/category.model.js
+++ back-end/model/category.model.js
@@ -1,17 +1,9 @@
 import { Schema, model } from "mongoose";
-<<<<<<< HEAD
-import { COLLECTIONS } from "../constant";
-
-export const CategorySchema = model(
-  "category",
-  Schema({
-=======
 import { COLLECTIONS } from "../constant/index.js";
 
 export const CategoryModel = model(
   "categorys",
   new Schema({
->>>>>>> main
     name: String,
     foodId: {
       type: [Schema.Types.ObjectId],
