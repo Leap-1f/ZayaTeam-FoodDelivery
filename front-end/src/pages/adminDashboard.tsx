8a218bfdb0dcba6a65a1c0b76ed96051e31853d2
batuka228 <batukagshhh@gmail.com>
2024-04-05 13:30:15 +0800

    bug zasav

--- front-end/src/pages/adminDashboard.tsx
+++ front-end/src/pages/adminDashboard.tsx
@@ -1,3 +1,35 @@
+import { useState, useEffect } from "react";
+import dotenv from "dotenv";
+import { Box } from "@mui/material";
+dotenv.config();
+const DEPLOYMENT_DB = process.env.DEPLOYMENT_URL;
+interface AllFoodDataType {
+  name: String;
+  image: String;
+  ingeredient: String;
+  price: Number;
+  discount: Number;
+  category: String;
+}
+
 export default function AdminDashboard() {
-  return <></>;
+  const [allOrders, setAllOrders] = useState<AllFoodDataType[]>([]);
+  const getAllOrders = async () => {
+    try {
+      const res = await fetch(`https://fooddelivery-back-end.vercel.app/order`);
+      const data = await res.json();
+      setAllOrders(data);
+      console.log(allOrders);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getAllOrders();
+  }, []);
+  return (
+    <>
+      <Box>sadasdas</Box>
+    </>
+  );
 }
