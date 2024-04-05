a031f3b50c29be674836d1889c51e54f59ef3611
batuka228 <batukagshhh@gmail.com>
2024-04-05 13:29:31 +0800

    bug

--- back-end/model/food.model.js
+++ back-end/model/food.model.js
@@ -1,25 +1,14 @@
 import { Schema, model } from "mongoose";
-<<<<<<< HEAD
-import { COLLECTIONS } from "../constant";
-
-export const FoodSchema = model(
-  "food",
-  Schema({
-=======
 import { COLLECTIONS } from "../constant/index.js";
 
 export const FoodSModel = model(
   "foods",
   new Schema({
->>>>>>> main
     name: String,
     image: String,
     ingeredient: String,
     price: Number,
     discount: Number,
-<<<<<<< HEAD
-=======
     category: String,
->>>>>>> main
   })
 );
--- back-end/model/user.model.js
+++ back-end/model/user.model.js
@@ -1,14 +1,8 @@
 import { Schema, model } from "mongoose";
 
-<<<<<<< HEAD
-export const UserSchema = model(
-  "user",
-  Schema({
-=======
 export const UserModel = model(
   "users",
   new Schema({
->>>>>>> main
     name: String,
     email: String,
     password: String,
